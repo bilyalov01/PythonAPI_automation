71800b7ff6777e9e6754f437556618ef910a17f6
bilyalov01 <aizar_aizar@mail.ru>
2024-01-27 22:38:36 +0600

    Soved ex8

---
+++ ex8.py
@@ -0,0 +1,18 @@
+import time
+import requests
+import json
+
+base_url = "https://playground.learnqa.ru/ajax/api/longtime_job"
+
+response = requests.get(base_url)
+token = response.json().get("token")
+sleep = response.json().get("seconds")
+params = {"token": token}
+print(sleep)
+
+while "result" not in response.text:
+    response = requests.get(base_url, params=params)
+    if response.json().get("status") == "Job is NOT ready":
+        time.sleep(sleep)
+print(response.status_code)
+print(response.text)
